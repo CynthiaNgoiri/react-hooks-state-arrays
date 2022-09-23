14113a3f68a5d340d737942ed164520e1b431e39
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-09-23 17:33:53 +0300

    completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -7,10 +7,28 @@ function SpicyFoodList() {
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
     console.log(newFood);
+    const newFoodList = [...foods, newFood];
+    setFoods(newFoodList);
+  }
+  function handleClick(id){
+    const newFoodList=foods.map((food)=>{
+      if (food.id === id){
+        return {
+          ...food, Heat: food.heatLevel +1,
+        };
+
+      }
+      else {
+        return food;
+      }
+      
+    });
+    setFoods(newFoodList);
+
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick ={handleClick}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
